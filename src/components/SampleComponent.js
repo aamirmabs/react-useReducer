9a1f4c9931e42652373a7c914f62db5eca8bd25f
aamyear <aamyear@gmail.com>
2021-06-05 11:26:35 +0400

    useReducer with object

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 
 function SampleComponent() {
   useEffect(() => {
@@ -8,9 +8,36 @@ function SampleComponent() {
     };
   }, []);
 
+  // const [state, setStateFunc] = useState(0);
+  // const [state, setStateFunc] = useReducer((accumulator, value) => {}, {});
+  // const [state, dispatch] = useReducer((state, value) => {}, {});
+
+  const [state, dispatch] = useReducer(
+    (state, initialValue) => {
+      switch (initialValue.type) {
+        case "A":
+          state = { ...state, counterA: state.counterA + 1 };
+          break;
+        case "B":
+          state = { ...state, counterB: state.counterB + 1 };
+          break;
+
+        default:
+          break;
+      }
+      return state;
+    },
+    { counterA: 0, counterB: 0 }
+  );
+
   return (
     <div>
       <h2>Sample Component</h2>
+      <h3>
+        A: {state.counterA} B: {state.counterB}
+      </h3>
+      <button onClick={() => dispatch({ type: "A" })}>A</button>
+      <button onClick={() => dispatch({ type: "B" })}>B</button>
     </div>
   );
 }
