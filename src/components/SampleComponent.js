f18b47904860dd2f825cf27af1feb61e4001f88b
aamyear <aamyear@gmail.com>
2021-06-15 07:53:58 +0400

    cart functionality implemented

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useReducer } from "react";
-import wishlistTrue from "../images/icons/wishlist-true.png";
-import wishlistFalse from "../images/icons/wishlist-false.png";
+import React, { useEffect, useReducer, useState } from "react";
 
 const productList = [
   {
@@ -30,171 +28,177 @@ const productList = [
   },
 ];
 
-function SampleComponent() {
-  // useEffect(() => {
-  //   console.log("useEffect run on render");
-  //   return () => {
-  //     console.log("useEffect cleanup");
-  //   };
-  // }, []);
+let initialCart = {
+  cart: {
+    prod1: {
+      name: "kala chasma",
+      price: 1000,
+      quantity: 1,
+    },
+    prod2: {
+      name: "kali aankhein",
+      price: 1250,
+      quantity: 1,
+    },
+    prod3: {
+      name: "laal chhadi",
+      price: 500,
+      quantity: 1,
+    },
+    prod4: {
+      name: "jalebi",
+      price: 50,
+      quantity: 1,
+    },
+    prod5: {
+      name: "japani joota",
+      price: 10000,
+      quantity: 1,
+    },
+  },
+  wishlist: ["prod1", "prod2"],
+  total: 0,
+};
 
+function SampleComponent() {
   // const [state, setStateFunc] = useState(0);
   // const [state, setStateFunc] = useReducer((accumulator, value) => {}, {});
   // const [state, dispatch] = useReducer((state, value) => {}, {});
 
-  const getProductFromID = (id) => {
-    const productIndex = productList.findIndex((product) => product.id === id);
-    const product = productList[productIndex];
+  const [state, dispatch] = useReducer((state, action) => {
+    const { type } = action;
+    const { id } = action.payload;
+    let obj = state.cart[id];
+    let newCart = {};
+    let newTotal = 0;
 
-    return product;
-  };
+    const calculateTotal = () => {
+      const cart = state.cart;
+      let newTotal = Object.keys(cart).reduce(function (previous, key) {
+        return previous + cart[key].price * cart[key].quantity;
+      }, 0);
+      return newTotal;
+    };
 
-  const initialCart = {
-    cart: [
-      {
-        id: "prod1",
-        productPosition: 0,
-        units: 0,
-        cost: 0,
-      },
-      {
-        id: "prod2",
-        productPosition: 1,
-        units: 0,
-        cost: 0,
-      },
-      {
-        id: "prod3",
-        productPosition: 2,
-        units: 0,
-        cost: 0,
-      },
-      {
-        id: "prod4",
-        productPosition: 3,
-        units: 0,
-        cost: 0,
-      },
-      {
-        id: "prod5",
-        productPosition: 4,
-        units: 0,
-        cost: 0,
-      },
-    ],
-    wishlist: ["prod1", "prod2"],
-  };
+    switch (type) {
+      case "ADD-TO-CART":
+        console.log("add to cart switch");
 
-  const [cartTracker, dispatch] = useReducer((prevCart, value) => {
-    let newCart = { ...prevCart };
-    // console.log("newCart before: ", newCart.cart);
+        newCart = {
+          ...state.cart,
+          [id]: { ...obj, quantity: obj.quantity + 1 },
+        };
+        newTotal = calculateTotal();
 
-    // check if product exists in the cart
-    const cartIndex = prevCart.cart.findIndex((item) => item.id === value.id);
+        return {
+          ...state,
+          cart: newCart,
+          total: newTotal,
+        };
 
-    switch (value.type) {
-      case "CART-INCREASE":
-        // console.log("CART-INCREASE" + Math.random().toFixed(2));
+      case "SUBTRACT-FROM-CART":
+        console.log("subtract from cart switch");
 
-        // console.log(cartIndex);
+        let newQuantity = obj.quantity <= 0 ? 0 : obj.quantity - 1;
 
-        // increase units of item if exists
-        let newUnits = prevCart.cart[cartIndex].units + 1;
-        newCart.cart[cartIndex].units = newUnits;
+        newCart = {
+          ...state.cart,
+          [id]: { ...obj, quantity: newQuantity },
+        };
 
-        // console.log("newCart after: ", newCart.cart);
-        return newCart;
+        return {
+          ...state,
+          cart: newCart,
+        };
 
-      case "CART-REMOVE":
-        newCart.cart.splice(cartIndex, 1);
-        // console.log("After remove: ", newCart);
-        return newCart;
+      case "REMOVE-FROM-CART":
+        console.log("remove from cart switch");
+        return state;
 
-      case "WISHLIST-TOGGLE":
-        const wishlistIndex = newCart.wishlist.findIndex(
-          (item) => item === value.id
-        );
+      case "TOGGLE-WISHLIST":
+        console.log("toggle wishlist switch");
+        return state;
 
-        if (wishlistIndex >= 0) {
-          console.log("Wishlist exists");
-          newCart.wishlist.splice(wishlistIndex, 1);
-        } else {
-          console.log("Wishlist NOT exists");
-          newCart.wishlist.push(value.id);
-        }
-        console.log(newCart.wishlist);
-        return newCart;
+      case "UPDATE-TOTAL":
+        newTotal = calculateTotal();
+        return {
+          ...state,
+          total: newTotal,
+        };
+
       default:
-        console.log("default");
-        return newCart;
+        break;
     }
   }, initialCart);
+
+  // setting a state to
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect run on render");
+    let cart = state.cart;
+    let total = Object.keys(cart).reduce(function (previous, key) {
+      return previous + cart[key].price * cart[key].quantity;
+    }, 0);
+    setCartTotal(total);
+  }, [state]);
 
   return (
     <div>
       <h2>Sample Component</h2>
-      <div className="layout">
-        <div className="products-list bordered">
-          <h3>Products</h3>
-          {productList.map((product) => {
-            const wishlistIcon = cartTracker.wishlist.includes(product.id)
-              ? "images/icons/wishlist-true.png"
-              : "images/icons/wishlist-false.png";
-            return (
-              <div className="product-item" key={product.id}>
-                <div>{product.name}</div>
-                <div>{product.price}</div>
-                <div>
-                  <button
-                    onClick={() =>
-                      dispatch({ type: "CART-INCREASE", id: product.id })
-                    }
-                  >
-                    +
-                  </button>
-                </div>
-                <div>
-                  <img
-                    onClick={() =>
-                      dispatch({ type: "WISHLIST-TOGGLE", id: product.id })
-                    }
-                    className="wishlist-icon"
-                    src={wishlistIcon}
-                    alt="wishlist"
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="cart">
-          <h3>Cart</h3>
-          {cartTracker.cart.map((product) => {
-            let position = product.productPosition;
-
-            return (
-              <div key={product.id}>
-                {productList[position].name} X {product.units} = {product.cost}{" "}
+      <div className="card-container">
+        {productList.map((product) => {
+          const { id, name, price } = product;
+          return (
+            <div className="card" key={id}>
+              <div className="product-name">{name}</div>
+              <div className="product-price">{price}</div>
+              <div className="product-status">
                 <button
-                  onClick={() => {
-                    dispatch({ type: "CART-REMOVE", id: product.id });
-                  }}
+                  onClick={() =>
+                    dispatch({ type: "ADD-TO-CART", payload: { id: id } })
+                  }
+                >
+                  +
+                </button>
+                <div className="cart-count">{state.cart[id].quantity}</div>
+                <button
+                  onClick={() =>
+                    dispatch({
+                      type: "SUBTRACT-FROM-CART",
+                      payload: { id: id },
+                    })
+                  }
+                >
+                  -
+                </button>
+                <button
+                  onClick={() =>
+                    dispatch({
+                      type: "REMOVE-FROM-CART",
+                      payload: { id: id },
+                    })
+                  }
                 >
                   Remove
                 </button>
+                <button
+                  onClick={() =>
+                    dispatch({
+                      type: "TOGGLE-WISHLIST",
+                      payload: { id: id },
+                    })
+                  }
+                >
+                  Wishlist
+                </button>
               </div>
-            );
-          })}
-          <h3>
-            TOTAL:{" "}
-            {cartTracker.cart.reduce((acc, val) => {
-              return acc + val.cost;
-            }, 0)}
-          </h3>
-        </div>
-        <div className="wishlist">
-          <h3>Wishlist</h3>
-        </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="card">
+        <h1>TOTAL: {cartTotal}</h1>
       </div>
     </div>
   );
